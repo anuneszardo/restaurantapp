4855d80c394741665290b2c1c4590fc7bbe4d715
anuneszardo <anuneszardo@gmail.com>
2019-04-19 16:28:57 -0300

    Update restaurantapp.py

--- restaurantapp.py
+++ restaurantapp.py
@@ -396,5 +396,4 @@ def internal_server_error(error):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
